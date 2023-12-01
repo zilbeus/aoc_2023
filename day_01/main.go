de101bbb7740e4fee9d889e4ed8d3969b9c37f50
zilbeus <118920399+zilbeus@users.noreply.github.com>
2023-12-01 10:43:30 +0200

    add solution to day 01 second part

--- day_01/main.go
+++ day_01/main.go
@@ -6,8 +6,53 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"unicode"
 )
+
+type StringDigit struct {
+	Word  string
+	Value string
+}
+
+var DigitStrings []StringDigit = []StringDigit{
+	{
+		Word:  "one",
+		Value: "1",
+	},
+	{
+		Word:  "two",
+		Value: "2",
+	},
+	{
+		Word:  "three",
+		Value: "3",
+	},
+	{
+		Word:  "four",
+		Value: "4",
+	},
+	{
+		Word:  "five",
+		Value: "5",
+	},
+	{
+		Word:  "six",
+		Value: "6",
+	},
+	{
+		Word:  "seven",
+		Value: "7",
+	},
+	{
+		Word:  "eight",
+		Value: "8",
+	},
+	{
+		Word:  "nine",
+		Value: "9",
+	},
+}
 
 func main() {
 	file, err := os.Open("input.txt")
@@ -20,7 +65,8 @@ func main() {
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		line := []rune(scanner.Text())
+		text := scanner.Text()
+		line := []rune(text)
 		var firstDigitFound bool
 		var lastDigitFound bool
 		var firstDigit rune
@@ -31,12 +77,22 @@ func main() {
 				firstDigit = line[i]
 				break
 			}
+			if isStringDigit(string(line[i:])) {
+				firstDigitFound = true
+				firstDigit = rune(getStringDigitValue(string(line[i:])))
+				break
+			}
 		}
 
 		for i := len(line) - 1; i >= 0; i-- {
 			if unicode.IsDigit(line[i]) {
 				lastDigitFound = true
 				lastDigit = line[i]
+				break
+			}
+			if isStringDigit(string(line[i:])) {
+				lastDigitFound = true
+				lastDigit = rune(getStringDigitValue(string(line[i:])))
 				break
 			}
 		}
@@ -60,4 +116,25 @@ func main() {
 	if err := scanner.Err(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func isStringDigit(input string) bool {
+	for _, el := range DigitStrings {
+		if strings.HasPrefix(input, el.Word) {
+			return true
+		}
+	}
+
+	return false
+}
+
+func getStringDigitValue(input string) rune {
+	for _, el := range DigitStrings {
+		if strings.HasPrefix(input, el.Word) {
+			return []rune(el.Value)[0]
+		}
+	}
+
+	log.Fatal("Should have found a value")
+	return 0
 }
